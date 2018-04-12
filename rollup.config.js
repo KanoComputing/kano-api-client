20a230204bac08e58ad681dbd5fab0c2b94cb07a
Marcus Hitchins <marcus7777@gmail.com>
2018-04-12 15:53:20 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default [{
+    input: 'kano-api-communication-lib.js',
+    output: {
+        file: 'dist/kano-api-communication-lib.js',
+        format: 'umd',
+        name: 'kanoApiCommunicationLib',
+    },
+}];
