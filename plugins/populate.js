b37de4d854613a524916ef67ac2cb178a0ac155e
Marcus Hitchins <marcus@kano.me>
2018-06-12 21:47:29 +0100

    plugin starting

--- plugins/populate.js
+++ plugins/populate.js
@@ -1,6 +1,12 @@
 export function populatePlugin(formats) {
     function populate (data, format) {
-        return JSON.parse(JSON.stringify(format), (key, value) => {
+        let theFormat
+        if (typeof format === 'function') {
+            theFormat = format(data);
+        } else {
+            theFormat = format;
+        }
+        return JSON.parse(JSON.stringify(theFormat), (key, value) => {
             if (typeof value === 'string' && /^[_a-z0-9\-.]*$/i.test(value) && Object.keys(data).indexOf(value) !== -1) {
                 return data[value]
             }
@@ -22,11 +28,17 @@ export default populatePlugin;
 
 const userFormat = {
     id: 'id',
+    name: 'username',
+};
+const userList = function (data) {
+    
 };
 
 const formats = {
     userById: userFormat,
     userByUsername: userFormat,
+    getListByIds: userList,
+    getFollows: userList,
 };
 
 client.addPlugin(populatePlugin(formats));
