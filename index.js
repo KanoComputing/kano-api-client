52c1c9a9f17cf7eff579f3c57cc2f1d0d9846dd7
Paul Varache <paul@kano.me>
2018-06-18 15:14:07 +0100

    Add more endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Client } from './lib/client.js';
 import { AccountClient } from './lib/account.js';
 import { UserClient } from './lib/user.js';
 import { ShareClient } from './lib/share.js';
+import { CommentClient } from './lib/comment.js';
 
-export { Client, AccountClient, UserClient, ShareClient };
-export default { Client, AccountClient, UserClient, ShareClient };
+export { Client, AccountClient, UserClient, ShareClient, CommentClient };
+export default { Client, AccountClient, UserClient, ShareClient, CommentClient };
